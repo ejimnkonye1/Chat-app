05367fc4bd3c7bd238d92fe7f3e188417d04148b
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-10-30 22:41:06 -0700

    the userprofile refuses to update

--- src/components/Userprofile.jsx
+++ src/components/Userprofile.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+
+import { useSelector } from 'react-redux'
 const Userprofile = () => {
-    const 
+    const username = useSelector((state) => state.username)
+    console.log(username)
+    const email = useSelector((state) => state.email)
   return (
     <div>
       <div>
-        <img src="" alt="" />
-      </div>
-      <h1>Confidence Chukwudi</h1>
-      <h1>Chukwudiconfidenceosinachi@gmail.com</h1>
+            <h1>Logged in as:</h1>
+            {username ? <h2>{username}</h2> : <h2>No Username Found</h2>}
+            {email ? <h3>{email}</h3> : <h3>No Email Found</h3>}
+        </div>
     </div>
   )
 }
