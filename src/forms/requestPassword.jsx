af18172cdccb55752055ba36c310cd4db9d705c3
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-01-19 23:10:14 +0100

    fix production issues

--- src/forms/requestPassword.jsx
+++ src/forms/requestPassword.jsx
@@ -3,7 +3,7 @@
 import {  useState } from 'react'
 import { sendPasswordResetEmail } from 'firebase/auth'
 import { auth } from '../Firebase'
-import { ErrorAlert } from '../alert'
+import { ErrorAlert } from '../Alert'
 
 
 const RequestPasswordform = ({setfogetPage}) => {
